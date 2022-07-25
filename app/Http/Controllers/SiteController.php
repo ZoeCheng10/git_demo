30be93478a25bea024479184aec09ba26e80d7df
史芝嘉 <migodoh@gmail.com>
2022-07-25 14:08:48 +0800

    02

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -2,22 +2,24 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class SiteController extends Controller
 {
-  public function hello(){
-      return "hello";
-  }
+    public function hello()
+    {
+        return "hello";
+    }
 
     public function yachi()
     {
         return "我上傳成功啦!";
     }
-    public function ss(){
-      sdfsffsl;
+    public function ss()
+    {
+        sdfsffsl;
     }
-       public function hello(){
-      sdfsffsl;
+    public function hello44()
+    {
+        sdfsffsl;
     }
+
 }
